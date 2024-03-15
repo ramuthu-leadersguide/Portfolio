12f75888e572717de545cb7dd75251501847de7a
ramuthu-leadersguide <w1903039@my.westminster.ac.uk>
2024-03-15 10:36:39 +0530

    first commit

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -5,22 +5,21 @@ const Address = () => {
     <>
       <p className="open-sans-font custom-span-contact position-relative">
         <i className="fa fa-map position-absolute"></i>
-        <span className="d-block">Address Point</span>No 435 Medakoratuwa ,
-        Kongala East Hakmana 81300.
+        <span className="d-block">Address Point</span>Panadura, Sri Lanka.
       </p>
       {/* End .custom-span-contact */}
 
       <p className="open-sans-font custom-span-contact position-relative">
         <i className="fa fa-envelope-open position-absolute"></i>
         <span className="d-block">mail me</span>{" "}
-        <a href="mailto:rasandilikshana@gmail.com">rasandilikshana@gmail.com</a>
+        <a href="mailto:rasandilikshana@gmail.com">ramuthuofficial@gmail.com</a>
       </p>
       {/* End .custom-span-contact */}
 
       <p className="open-sans-font custom-span-contact position-relative">
         <i className="fa fa-phone-square position-absolute"></i>
         <span className="d-block">call me</span>{" "}
-        <a href="Tel: +94702294951">+94 702 294 951</a>
+        <a href="Tel: +94702294951">+94 773 698 632</a>
       </p>
       {/* End .custom-span-contact */}
     </>
